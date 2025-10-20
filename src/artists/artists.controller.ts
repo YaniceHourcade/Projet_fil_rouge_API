c935f5a82720f7982c26929a2e9297b830d2b5a2
yanice <yanice.hourcade@ynov.com>
2025-10-20 14:42:15 +0200

    start

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,17 +1,27 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
+import { Artist } from '@prisma/client';
+
+type ArtistWithRelations = Artist & {
+  albums: { id: number; title: string; year: number; artistId: number }[];
+  concerts: { id: number; location: string; date: Date; artistId: number }[];
+};
 
 @Controller('artists')
 export class ArtistsController {
   constructor(private readonly artistsService: ArtistsService) {}
 
+  // GET /artists
   @Get()
-  async findAll(): Promise<(Artist & { albums: any[]; concerts: any[] })[]> {
+  async findAll(): Promise<ArtistWithRelations[]> {
     return this.artistsService.findAll();
   }
 
+  // POST /artists
   @Post()
-  create(@Body() body: { name: string; genre: string }) {
+  async create(
+    @Body() body: { name: string; genre: string },
+  ): Promise<ArtistWithRelations> {
     return this.artistsService.create(body);
   }
 }
--- src/artists/artists.service.ts
+++ src/artists/artists.service.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Artist } from '@prisma/client';
+
+type ArtistWithRelations = Artist & {
+  albums: { id: number; title: string; year: number; artistId: number }[];
+  concerts: { id: number; location: string; date: Date; artistId: number }[];
+};
 
 @Injectable()
 export class ArtistsService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-  async findAll() {
-    return this.prismaService.artist.findMany({ include: { albums: true, concerts: true } });
+  async findAll(): Promise<ArtistWithRelations[]> {
+    return this.prismaService.artist.findMany({
+      include: { albums: true, concerts: true },
+    });
   }
 
-  async create(data: { name: string; genre: string }) {
-    return this.prismaService.artist.create({ data });
+  async create(data: { name: string; genre: string }): Promise<ArtistWithRelations> {
+    return this.prismaService.artist.create({
+      data,
+      include: { albums: true, concerts: true },
+    });
   }
 }
