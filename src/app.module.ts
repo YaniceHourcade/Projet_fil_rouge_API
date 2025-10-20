47d6994b65690f431f041a8cadaa45be1377e559
dnoemie <noemie.dublanc@ynov.com>
2025-10-20 14:40:50 +0200

    push

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ArtistsModule } from './artists/artists.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ArtistsModule],
+  
 })
+
+@Module({
+  imports: [UsersModule],
+})
+
 export class AppModule {}
