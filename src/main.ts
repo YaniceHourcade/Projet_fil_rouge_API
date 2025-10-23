f4c7b7636decf1c925ee4f2170bd79fb06f72965
dnoemie <noemie.dublanc@ynov.com>
2025-10-23 09:51:08 +0200

    push documentation api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+    .setTitle('Mon API projet fil rouge')
+    .setDescription('Documentation de mon API NestJS avec OpenAPI')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
   console.log(`🚀 Application is running on: http://localhost:3000`);
+
 }
 bootstrap();
